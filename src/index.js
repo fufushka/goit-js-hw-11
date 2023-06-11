20223251215ad03b7fdb2519278d825817a0bb6b
fufushka <dimaukropdred@gmail.com>
2023-06-11 23:01:37 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import axios from 'axios';
-import getPhotos from './js/photoApi';
+
+import { getPhotos } from './js/photoApi';
 const lightbox = new SimpleLightbox('.lightbox', {
   delay: 250,
 });
+
 const refs = {
   form: document.querySelector('.search-form'),
   cards: document.querySelector('.cards'),
@@ -39,11 +40,10 @@ async function onSubmitForm(event) {
       );
       return;
     }
-
+    refs.paginationBtn.classList.remove('is-hidden');
     const markup = generateMarkup(photos);
     refs.cards.innerHTML = markup;
 
-    refs.paginationBtn.classList.remove('is-hidden');
     Notiflix.Notify.success(`Hooray! We found ${photos.totalHits} images.`);
     lightbox.refresh();
   } catch (error) {
@@ -104,29 +104,27 @@ function createMarkup({
 
 async function searchImages() {
   page += 1;
-
   return await getPhotos(searchQuery, page);
 }
 
 function generateMarkup(photos) {
-  const maxPage = Math.ceil(photos.totalHits / 40);
-
-  console.log(maxPage);
-  const nextPage = page;
-  console.log(nextPage);
-  if (nextPage > maxPage) {
-    refs.paginationBtn.classList.add('is-hidden');
-    Notiflix.Notify.info(
-      "We're sorry, but you've reached the end of search results."
-    );
-    return '';
-  }
-
   const markup = photos.hits.reduce(
     (markup, currentPhoto) => markup + createMarkup(currentPhoto),
     ''
   );
 
+  const maxPage = Math.ceil(photos.totalHits / 40);
+  console.log(maxPage);
+  const nextPage = page + 1;
+  console.log(nextPage);
+  if (nextPage > maxPage) {
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+    refs.paginationBtn.classList.add('is-hidden');
+
+    // return markup;
+  }
   return markup;
 }
 
--- src/js/photoApi.js
+++ src/js/photoApi.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/';
+
 const API_KEY = '37180259-a7b74ce7fbdff4a67e50e8712';
-async function getPhotos(query, page) {
+
+export async function getPhotos(query, page) {
   const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
 
   const response = await axios.get(url);
 
   return response.data;
 }
-export default getPhotos;
